8e50ac870dc990e90ab9637a35c4ab059d2ab75d
Nikita <seraser90@gmail.com>
2024-08-27 15:41:10 +0300

    complete 2_2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,27 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1  // your code
+let dice2  // your code
+let isWinningDouble = false  // your code
 
-/*
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
+while(isWinningDouble === false) {
+  dice1 = Math.ceil(Math.random() * 6);
+  dice2 = Math.ceil(Math.random() * 6);
+  console.log('Первый бросок: '  + dice1)
+  console.log('Второй бросок: '  + dice2)
+  if ((dice1 === dice2) && (dice1 > 3)) {
+    isWinningDouble = true
+    console.log('Выигрышный дубль!')
 } else {
-  console.log('Не выигрышный дубль.')
+    isWinningDouble = false
+    console.log('Не выигрышный дубль.')
 }
-*/
+}
+// if (isWinningDouble) {
+//   console.log('Выигрышный дубль!')
+// } else {
+//   console.log('Не выигрышный дубль.')
+// }
+
+
