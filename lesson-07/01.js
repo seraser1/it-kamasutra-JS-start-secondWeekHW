0ebd474e88d546f0ed9f34efbc226e4540f00096
Nikita <seraser90@gmail.com>
2024-10-03 13:47:48 +0300

    restailing mother father

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,16 +6,37 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords(str) {
-  const words  = str.split(' ');
-  const result = [];
-  for (let i = 0; i < words.length; i++) {
-    // const word = words[i];
-    const resEl = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase()
-    result.push(resEl)
+// function capitalizeWords(str) {
+//   const words  = str.split(' ');
+//   const result = [];
+//   for (let i = 0; i < words.length; i++) {
+//     // const word = words[i];
+//     const resEl = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase()
+//     result.push(resEl)
+//   }
+//   return result.join(' ')
+// } 
+
+// function capitalizeWords(string) {
+//   let str = ''
+//   str += string[0].toUpperCase()
+//   for (let i = 1; i < string.length; i++) {
+//     if(string[i - 1] === ' ') str += string[i].toUpperCase()
+//       else str += string[i]
+//   }
+//   return str
+// } 
+
+function capitalizeWords(string) {
+  let str = '';
+  for (let i = 0; i < string.length; i++) {
+    if(i === 0 || string[i - 1] === ' ') str += string[i].toUpperCase()
+      else str += string[i]
   }
-  return result.join(' ')
+  return str
 } 
+
+
 
 // function capitalizeWords(str) {
 //   return str.split(" ") // Разбиваем строку на массив слов
