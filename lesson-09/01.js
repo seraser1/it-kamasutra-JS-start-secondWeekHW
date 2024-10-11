a9450eff236fa3982c1c5aaa29ff2953fff4642e
Nikita <seraser90@gmail.com>
2024-10-11 03:58:28 +0300

    complete 9_1

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -13,14 +13,29 @@
 Функция `filter` должна возвращать новый массив, содержащий только те элементы `array`, для которых `callback` возвращает `true`.
 */
 
-/* Пример использования:
-const numbers = [1, 2, 3, 4, 5]
+//  Пример использования:
+// const numbers = [1, 2, 3, 4, 5]
 
-const oddNumbers = filter(numbers, (element, index) => {
-  return element % 2 !== 0
-});
+// const oddNumbers = filter(numbers, (element, index) => {
+//   return element % 2 !== 0
+// });
 
-console.log(oddNumbers) // Должен вывести: [1, 3, 5]
-*/
+// console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 
-const filter = () => {}
+
+// const filter = () => {}
+
+
+function filter (array, callback) {
+    const res = [];
+    for (let i = 0; i < array.length; i++) {
+      if (callback(array[i], i))  res.push(array[i]);
+    }
+    return res;
+  }
+
+
+
+
+
+
